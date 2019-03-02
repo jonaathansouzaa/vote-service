a223d906e6eca1ebad04c97dac3c3c2f52f7797c
Jonathan Souza Santos <jonathan.programmer@gmail.com>
2019-03-02 11:31:54 -0300

    Commit initial

---
+++ src/main/java/com/vote/voteservice/VoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vote.voteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vote/voteservice/VoteServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vote.voteservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VoteServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
