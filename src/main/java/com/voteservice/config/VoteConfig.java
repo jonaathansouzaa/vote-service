2c970f76b8e7359e3233f380de12383e73717807
Jonathan Souza Santos <jonathan.programmer@gmail.com>
2019-03-27 17:48:14 -0300

    Remove RabbitMQ

--- src/main/java/com/voteservice/config/VoteConfig.java
+++ src/main/java/com/voteservice/config/VoteConfig.java
@@ -1,8 +1,6 @@
 package com.voteservice.config;
 
-import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -36,9 +34,9 @@ public class VoteConfig {
 		return queueName;
 	}
 	
-	@Bean
-    public Queue queue() {
-        return new Queue(queueName, true);
-    }
+//	@Bean
+//    public Queue queue() {
+//        return new Queue(queueName, true);
+//    }
 	
 }
--- src/test/java/com/voteservice/producer/VoteProducerTest.java
+++ src/test/java/com/voteservice/producer/VoteProducerTest.java
@@ -1,38 +1,27 @@
 package com.voteservice.producer;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-
-@RunWith(MockitoJUnitRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
 public class VoteProducerTest {
 
-	@Mock
-	private RabbitTemplate rabbitTemplate;
-	
-	@Mock
-	private Queue queue;
-	
-	@InjectMocks
-	private VoteProducer voteProducer;
-	
-	@Test
-	public void testIfIsProducingTheMessage() {
-		String message = "The session is closed.";
-		
-		String queueName = "queueName";
-		when(queue.getName()).thenReturn(queueName);
-		voteProducer.produceMessage(message);
-		
-		verify(queue).getName();
-		verify(rabbitTemplate).convertAndSend(queueName, message);
-	}
+//	@Mock
+//	private RabbitTemplate rabbitTemplate;
+//	
+//	@Mock
+//	private Queue queue;
+//	
+//	@InjectMocks
+//	private VoteProducer voteProducer;
+//	
+//	@Test
+//	public void testIfIsProducingTheMessage() {
+//		String message = "The session is closed.";
+//		
+//		String queueName = "queueName";
+//		when(queue.getName()).thenReturn(queueName);
+//		voteProducer.produceMessage(message);
+//		
+//		verify(queue).getName();
+//		verify(rabbitTemplate).convertAndSend(queueName, message);
+//	}
 	
 }
